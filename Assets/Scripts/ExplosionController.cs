44d407499c519a48e0c690ea826cd7f3e69b546e
SergiGnvs <secaso@alumnatflorida.es>
2026-05-01 22:24:58 +0200

    Completado Ejercicio 8

--- Assets/Scripts/ExplosionController.cs
+++ Assets/Scripts/ExplosionController.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class ExplosionController : MonoBehaviour
 {
+    [SerializeField] int fuerzaExplosion;
+    [SerializeField] int radioExplosion;
+    [SerializeField] int upwardsModifier;
+    private Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 explosionPosition = transform.position;
+
+            Collider[] colliders = Physics.OverlapSphere(explosionPosition, radioExplosion);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                if (rb != null)
+                    rb.AddExplosionForce(fuerzaExplosion, explosionPosition, radioExplosion, 3.0f);
+            }
+        }
     }
 }
